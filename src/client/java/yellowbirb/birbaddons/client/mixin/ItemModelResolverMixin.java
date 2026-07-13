e24aab8cb1916e3db1829f5ffe3f118b9e6100d2
lzietz <luca.zietz@edu.rptu.de>
2026-07-13 08:39:11 +0200

    No more Swap Animation on Drills

---
+++ src/client/java/yellowbirb/birbaddons/client/mixin/ItemModelResolverMixin.java
@@ -0,0 +1,29 @@
+package yellowbirb.birbaddons.client.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.client.renderer.item.ItemModelResolver;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemModelResolver.class)
+public class ItemModelResolverMixin {
+    @WrapMethod(method = "swapAnimationScale")
+    public float swapAnimationScale(ItemStack stack, Operation<Float> original) {
+        if (true /* TODO: isEnabled */) {
+            CustomData customData = stack.get(DataComponents.CUSTOM_DATA);
+            if (customData != null) {
+                Tag tag = customData.copyTag().get("id");
+                if (tag != null) {
+                    if (tag.toString().toLowerCase().contains("drill".toLowerCase())) {
+                        return 0.0F;
+                    }
+                }
+            }
+        }
+        return original.call(stack);
+    }
+}
